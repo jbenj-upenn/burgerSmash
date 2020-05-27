e2fc96597153168be0b20da6621d8bee4e3d3fab
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-05-27 11:23:41 -0400

    refined seeds file, working towards running them in mysql from the command line

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
--- Drops the programming_db if it already exists --
+-- Drops the burgers_db if it already exists --
 DROP DATABASE IF EXISTS burgers_db;
 
--- Created the DB "wizard_schools_db" (only works on local connections)
+-- Created the DB burgers_db (only works on local connections)
 CREATE DATABASE burgers_db;
 
--- Use the DB wizard_schools_db for all the rest of the script
+-- Use the DB burgers_db for all the rest of the script
 USE burgers_db;
 
 -- Created the table "schools"
@@ -12,27 +12,8 @@ CREATE TABLE burgers (
     id int AUTO_INCREMENT NOT NULL,
     burger_name varchar(30) NOT NULL,
     smashed BOOLEAN DEFAULT false,
-    todaydate TIMESTAMP,
+    dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(id)
 );
 
--- Inserted a set of records into the table
-INSERT INTO
-    schools (name)
-VALUES
-    ("Hogwarts School of Witchcraft");
 
-INSERT INTO
-    schools (name)
-VALUES
-    ("Castelobruxo");
-
-INSERT INTO
-    schools (name)
-VALUES
-    ("Durmstrang Institute");
-
-INSERT INTO
-    schools (name)
-VALUES
-    ("Beauxbatons Academy of Magic");
