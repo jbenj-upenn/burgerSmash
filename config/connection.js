ac83a3a01de036995f6552737c220b7c81d38333
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-05-27 13:19:35 -0400

    committing changes to files and folders

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+//connect Node to MySql
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+//Export connection
+module.exports = connection;
---
+++ models/burger.js
@@ -0,0 +1,4 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+//Include CRUD functionality
