681b0d126df281077ed555230d981511a8d02b83
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-05-29 10:59:10 -0400

    Heroku configurations with JawsDB added

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,8 @@ if (process.env.JAWSDB_URL) {
   host: "localhost", 	
   port: 3306,
   user: "root", 
-  password: "", 
-  database: "burgers_db" 
+  password: "hacktheplanet", 
+  database: "todoagain_db" 
 });
 
 //jawsDB info for Heroku: host, user, pw, db
