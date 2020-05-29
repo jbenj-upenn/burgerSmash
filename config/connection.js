3345b2dc65a9e26b6b729bde200762e068a0d8ba
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-05-29 16:47:26 -0400

    connection.js updates for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 //connect Node to MySql
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+ } else {
+   connection = mysql.createConnection({
   host: "localhost", 	
   port: 3306,
   user: "root", 
   password: "", 
   database: "burgers_db" 
 });
-
-//jawsDB info for Heroku: host, user, pw, db
-//tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
-//"i29477rb1dpha62z",
-//h1up5hb7x528pers
-//ej1v1qqxdkfeeg9g
+ };
 
 connection.connect(function(err) {
   if (err) {
